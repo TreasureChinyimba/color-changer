af05ad57870727dd61cd4c765b29abe85be05da7
Treasure Chinyimba <Treasurearthur4@gmail.com>
2024-02-08 17:49:03 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//yellow
+const yellowBtn = document.querySelector('.yellowBtn');
+const background = document.querySelector('.container');
+yellowBtn.addEventListener('click',()=>{
+       background.style = 'background-color: yellow;';
+})
+
+//black
+const blackBtn = document.querySelector('.blackBtn');
+blackBtn.addEventListener('click',()=>{
+       background.style = 'background-color: black;';
+})
+
+//purple
+const purpleBtn = document.querySelector('.purpleBtn');
+purpleBtn.addEventListener('click',()=>{
+       background.style = 'background-color: purple;';
+})
+
+//pink
+const pinkBtn = document.querySelector('.pinkBtn');
+pinkBtn.addEventListener('click',()=>{
+       background.style = 'background-color: pink;';
+})
